b0056308e31b25533c231fb9fcbdb7e2ae0af3ff
Savio Kerber <savioazure@gmail.com>
2022-04-27 15:55:32 +0200

    fixed and working

--- 01-hello/hello.py
+++ 01-hello/hello.py
@@ -3,12 +3,14 @@
 
 import argparse
 
-
-def main():
+def get_args():
     parser = argparse.ArgumentParser(description= 'Say hello')
     parser.add_argument('-n', '--name', metavar='name', default='World', help='Name to greet')
-    args = parser.parse_args()
-    #print (args)
+
+    return parser.parse_args()
+
+def main():
+    args = get_args()
     print ('Hello, ' + args.name + '!')
 
 if __name__ == '__main__':
