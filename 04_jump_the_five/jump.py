8f36add4d14080a9239cce68452cd11bec54aaec
administrador <savioazure@gmail.com>
2023-11-04 14:57:55 +0100

    1st solution to ex4

--- 04_jump_the_five/jump.py
+++ 04_jump_the_five/jump.py
@@ -28,19 +28,16 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    str_arg = args.arg
-    int_arg = args.int
-    file_arg = args.file
-    flag_arg = args.on
-    pos_arg = args.positional
+    jumper = {'1': '9', '2': '8', '3': '7', '4': '6', '5': '0',
+              '6': '4', '7': '3', '8': '2', '9': '1', '0': '5'}
 
-    print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
+    for char in args.text:
+        print(jumper.get(char, char), end= '')
+    print()
 
 
 # --------------------------------------------------
 if __name__ == '__main__':
     main()
+
+
