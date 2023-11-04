7e4e7ba98afa8640ec23ffbfb8e1727af003fc6d
administrador <savioazure@gmail.com>
2023-11-04 22:22:23 +0100

    2nd solution to ex4

--- 04_jump_the_five/jump.py
+++ 04_jump_the_five/jump.py
@@ -31,9 +31,13 @@ def main():
     jumper = {'1': '9', '2': '8', '3': '7', '4': '6', '5': '0',
               '6': '4', '7': '3', '8': '2', '9': '1', '0': '5'}
 
+    # for char in args.text:
+    #     print(jumper.get(char, char), end= '')
+    # print()
+    new_text = ''
     for char in args.text:
-        print(jumper.get(char, char), end= '')
-    print()
+        new_text += jumper.get(char, char)    
+    print(new_text)
 
 
 # --------------------------------------------------
